76eb63b39af0cd9af7b6df3d9ba065bd3636f50b
tahaca1 <tahabenrayanaca@gmail.com>
2026-01-14 11:11:20 +0100

    chore(restructure): flatten project structure, archive artifacts, add .gitignore entries

--- open-library-app-final/open-library-app/src/app/models/book.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Book {
-  key: string;
-  title: string;
-  edition_count: number;
-  cover_id: number;
-  first_publish_year: number;
-  subtitle: string;
-  description: string;
-}
