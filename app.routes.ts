179922a206d3c2e3e13051e764c66bcca2599c68
tahaca1 <tahabenrayanaca@gmail.com>
2026-01-14 11:16:28 +0100

    chore(restructure): remove duplicate top-level angular files (kept canonical copies in angular/src)

--- app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { BookDetailsComponent } from './components/book-details/book-details.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'book/:id', component: BookDetailsComponent },
-  { path: '**', redirectTo: '' }
-];
--- head-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-head-bar',
-  standalone: true,
-  templateUrl: './head-bar.component.html',
-  styleUrls: ['./head-bar.component.css']
-})
-export class HeadBarComponent {}
